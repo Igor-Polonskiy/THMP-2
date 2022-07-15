23d2387707e3cd6a56e0b1caa7ed9e2acc6b4211
Polonskiy Igor <barlogfire@ya.ru>
2022-07-15 10:18:02 +0300

    wrappers

---
+++ Сложность_1/2/js-data/doll_2_script.js
@@ -0,0 +1,266 @@
+
+(() => {
+    // doll_2_
+
+    // Собираются элемненты в переменные
+    const wrapper = document.querySelector('.doll_2_Wrapper');
+
+    const soundBtn = wrapper.querySelector('.doll_2_MuteSound');
+    const divSet = wrapper.querySelector('.doll_2_Set');
+    // const counter = wrapper.querySelector('.itemCount');
+    const container = wrapper.querySelector('.doll_2_Container');
+    const sliderwrapper = wrapper.querySelector('.doll_2_SliderWrapper');
+    const resetBtn = wrapper.querySelector('.doll_2_ResetBtn');
+    const house = wrapper.querySelector('.house');
+    const winHousePicture = wrapper.querySelector('.winHousePicture');
+    const fullScreenBtn = wrapper.querySelector('.doll_2_FullScreenBtn');
+    const winSound = document.getElementById('winSound');
+    const feedBackdoll_2_ = wrapper.querySelector('.smiledoll_2');
+
+
+
+    // Собирается элементы в переменные, создаются вспомогательные переменные
+
+
+    let windoll_2_ = false;
+    // Обработчик кнопки "Вернуть к исходному состоянию"
+    resetBtn.addEventListener('click', reset);
+
+    function reset() {
+        console.log('reset')
+    }
+    // Обработчик кнопки "Полный экран"
+
+    fullScreenBtn.addEventListener('click', function (event) {
+        if (wrapper.classList.contains('doll_2_Content-blocker')) {
+            wrapper.classList.remove('doll_2_Content-blocker');
+            document.getElementsByTagName('body')[0].style.overflowY = null;
+        }
+        else {
+            wrapper.classList.add('doll_2_Content-blocker');
+            document.getElementsByTagName('body')[0].style.overflowY = "hidden";
+        }
+        if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+        } else {
+            document.documentElement.requestFullscreen();
+        }
+    }, false);
+
+    let soundOn = false;
+    soundBtn.addEventListener('click', soundChanger);
+    function soundChanger() {
+        soundOn = !soundOn;
+        if (soundOn) {
+            soundBtn.src = "Images_1/doll_2_img/onSound.svg";
+        }
+        else {
+            soundBtn.src = "Images_1/doll_2_img/mute.svg";
+        }
+    }
+
+    function playSound(audio) {
+        soundOn && audio.play();
+    }
+
+    function changeStylesAndAppend(dropPlace, draggingElem) {
+        draggingElem.style.position = 'relative ';
+        draggingElem.style.zIndex = null;
+        draggingElem.style.top = null;
+        draggingElem.style.left = null;
+        dropPlace.appendChild(draggingElem);
+    }
+
+   
+
+    let draggingItem;
+    let elemBelow;
+
+    function mouseDown(event) {
+        if (event.button !== 0) return;
+        draggingItem = event.target;
+        draggingItem.style.cursor = "url(Images_1/doll_2_img/cursor.png), auto";
+        const elemDraggingBanBorder = container;//элемент за границы которого запрещён вылет перетаскиваемой фигуры
+        const elemDraggingStartPlace = divSet;  //элемент первоначального расположения перетаскиваемых фигур (стартовое состояние)
+
+        draggingItem.style.touchAction = 'none'; //ОБЯЗАТЕЛЬНОЕ УСЛОВИЕ(МОЖНО УБРАТЬ И ПРОПИСАТЬ В СТИЛЬ САМОМУ ОБЪЕКТУ) 
+        let shiftX = event.clientX - draggingItem.getBoundingClientRect().left;
+        let shiftY = event.clientY - draggingItem.getBoundingClientRect().top;
+
+        // ЛИММИТЫ КООРДИНАТ ОГРАНИЧИВАЮЩИЕ ВЫЛЕТ ПЕРЕТАСКИВАЕМОГО ЭЛЕМЕНТА ЗА БЛОК
+        //  (ПО УМОЛЧАНИЮ interact_zadanie - РОДИТЕЛЬ ВАШЕГО БЛОКА)
+        // let limits = {
+        //     top: elemDraggingBanBorder.offsetTop,
+        //     right: elemDraggingBanBorder.offsetWidth + elemDraggingBanBorder.offsetLeft,
+        //     bottom: elemDraggingBanBorder.offsetHeight + elemDraggingBanBorder.offsetTop,
+        //     left: elemDraggingBanBorder.offsetLeft
+        // };
+
+        draggingItem.style.position = 'absolute';
+        draggingItem.style.zIndex = 1000;
+        document.body.appendChild(draggingItem);
+
+        moveAt(event.pageX, event.pageY);
+
+        function moveAt(pageX, pageY) {
+            draggingItem.style.left = pageX - shiftX + 'px';
+            draggingItem.style.top = pageY - shiftY + 'px';
+        }
+
+        elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        let currentDroppable = null;
+        let clickWithoutMove = true;
+        function onMouseMove(event) {
+            // let newLocation = {
+            //     x: limits.left,
+            //     y: limits.top
+            // };
+            // if (event.pageX > limits.right) {
+            //     newLocation.x = limits.right;
+            // }
+            // else if (event.pageX > limits.left) {
+            //     newLocation.x = event.pageX;
+            // }
+            // if (event.pageY > limits.bottom) {
+            //     newLocation.y = limits.bottom;
+            // }
+            // else if (event.pageY > limits.top) {
+            //     newLocation.y = event.pageY;
+            // }
+
+            clickWithoutMove = false
+            // moveAt(newLocation.x, newLocation.y);
+            moveAt(event.pageX, event.pageY);
+            if (!event.path.includes(draggingItem)) {
+                window.addEventListener('pointerup', moveOut);
+            }
+            if (event.path.includes(draggingItem)) {
+                window.removeEventListener('pointerup', moveOut);
+            }
+
+            draggingItem.hidden = true;
+            elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+            draggingItem.hidden = false;
+
+            if (!elemBelow) return;
+
+            // ОБРАБОТКА СОБЫТИЯ НАХОЖДЕНИЯ НАД БЛОКОМ И ВЫЛЕТА ИЗ НЕГО (ПО НЕОБХОДИМИОСТИ)
+
+
+            let droppableBelow = elemBelow.closest('.houseElement'); // БЕРЁМ НУЖНЫЙ БЛОК 
+
+            if (currentDroppable != droppableBelow) {
+                if (currentDroppable) {
+                    // ЛОГИКА ОБРАБОТКИ ПРОЦЕССА "ВЫЛЕТА" ИЗ DROPPABLE
+                    leaveDroppable(currentDroppable);
+                }
+                currentDroppable = droppableBelow;
+                // ЛОГИКА ОБРАБОТКИ ПРОЦЕССА, КОГДА МЫ "ВЛЕТАЕМ" В ЭЛЕМЕНТ
+                if (currentDroppable) {
+                    enterDroppable(currentDroppable);
+                }
+            }
+        }
+
+        // КОГДА НАД ВЫБРАННЫМ БЛОКОМ
+        function enterDroppable(currentDroppable) {
+            currentDroppable.classList.add('houseElementsShadowRed');
+        }
+        // КОДА ВЫЛЕТЕЛИ ИЗ БЛОКА
+        function leaveDroppable(currentDroppable) {
+            currentDroppable.classList.remove('houseElementsShadowRed');
+
+        }
+        document.addEventListener('pointermove', onMouseMove);
+
+
+        // КОГДА ВО ВРЕМЯ ПЕРЕТАСКИВАНИЯ КУРСОР ВЫНЕСЛИ ЗА ПРЕДЕЛЫ ОКНА БРАУЗЕРА И ОТПУСТИЛИ ЗАХВАТ ЭЛЕМЕНТА
+
+
+        function moveOut(e) {
+            smoothTransition(draggingItem)
+            setTimeout(() => changeStylesAndAppend(elemDraggingStartPlace, draggingItem), 1000)
+            window.removeEventListener('pointerup', moveOut);
+            document.removeEventListener('pointermove', onMouseMove);
+        }
+
+        // КОГДА КУРСОР В ЗОНЕ ДЛЯ ПЕРЕТАСКИВАНИЙ И ПОЛЬЗОВАТЕЛЬ ОТПУСТИЛ ЗАХВАТ ЭЛЕМЕНТА
+        draggingItem.addEventListener('pointerup', onpointerup)
+        function onpointerup() {
+            startAction = true;
+            if (clickWithoutMove) {
+                smoothTransition(draggingItem)
+                setTimeout(() => changeStylesAndAppend(elemDraggingStartPlace, draggingItem), 1000)
+            }
+
+            document.removeEventListener('pointermove', onMouseMove);
+
+            // ЛОГИКА ОБРАБОТКИ ПОПАДАНИЯ НА НУЖНЫЙ БЛОК И НАОБОРОТ
+            if (elemBelow.classList.contains('houseElement')) {
+                changeStylesAndAppend(elemBelow, draggingItem);
+                house.childNodes.forEach(item => {
+                    item.classList?.remove('houseElementsShadowRed');
+                });
+                houseGameResult();
+            } else {
+                smoothTransition(draggingItem)
+                setTimeout(() => changeStylesAndAppend(elemDraggingStartPlace, draggingItem), 1000)
+
+            }
+            draggingItem.removeEventListener('pointerup', onpointerup)
+        };
+
+        function smoothTransition(draggingElem) {
+            //document.body.style.pointerEvents = 'none'
+            elemsdoll_2_.forEach((e) => {
+                e.removeEventListener('pointerdown', mouseDown);
+            });
+            let coordX,
+                coordY
+            draggingElem.classList.add('dragTransition')
+            coordX = elemDraggingStartPlace.getBoundingClientRect().left + elemDraggingStartPlace.getBoundingClientRect().width
+            coordY = elemDraggingStartPlace.getBoundingClientRect().top + window.pageYOffset
+            draggingElem.style.left = `${coordX}px`
+            draggingElem.style.top = `${coordY}px`
+            setTimeout(() => {
+                draggingElem.classList.remove('dragTransition')
+                //document.body.style.pointerEvents = 'auto'
+                elemsdoll_2_.forEach((e) => {
+                    e.addEventListener('pointerdown', mouseDown);
+                });
+
+            }, 1000)
+        }
+
+
+    };
+
+
+
+
+
+    // обработка положительного или отрицательного результата сбора домика
+
+   
+
+
+
+
+    function winTextSwitcherdoll_2_(feedBack) {
+        if (windoll_2_) {
+            if (wrapper.querySelector('.doll_2_Well_done').classList.contains('fadedoll_2_')) {
+
+                wrapper.querySelector('.doll_2_Well_done').classList.remove('fadedoll_2_');
+                wrapper.querySelector('.doll_2_Well_done').classList.add('onViewdoll_2_');
+                feedBackdoll_2_.src = feedBack;
+            }
+            else if (wrapper.querySelector('.doll_2_Well_done').classList.contains('onViewdoll_2_')) {
+                wrapper.querySelector('.doll_2_Well_done').classList.remove('onViewdoll_2_');
+                wrapper.querySelector('.doll_2_Well_done').classList.add('fadedoll_2_');
+
+            }
+        }
+
+    }
+})()
---
+++ Сложность_3/7/js-data/doll_7_script.js
@@ -0,0 +1,294 @@
+(() => {
+
+    
+    const wrapper = document.querySelector('.doll_7_wrapper');
+
+    const divMain = wrapper.querySelector('.doll_7_Main');
+    const resetBtn = wrapper.querySelector('.doll_7_ResetBtn');
+    const soundBtn = wrapper.querySelector('.doll_7_MuteSound');
+    const soundBtfullScreenBtn = wrapper.querySelector('.doll_7_FullScreenBtn');
+    const container = wrapper.querySelector('.container');
+    const insideBox = wrapper.querySelector('.doll_7_insideBox');
+    const contentBlocker = 'doll_7_ContentBlocker';
+    const well_done = wrapper.querySelector('.doll_7_Well_done');
+
+   
+    // hide" draggable="true" ondrop="return false"
+
+   
+
+
+
+
+
+
+    soundBtn.addEventListener('click', soundChanger);
+    let soundOn = false;
+
+    function soundChanger() {
+        soundOn = !soundOn;
+        if (soundOn) {
+            soundBtn.src = "Images_1/doll_7_img/onSound.svg";
+        } else {
+            soundBtn.src = "Images_1/doll_7_img/mute.svg";
+        }
+    }
+
+    function playSound(audio) {
+        soundOn && audio.play();
+    }
+
+    
+
+    
+
+    let winOpenBox = false;
+
+    // Обработчик кнопки "Вернуть к исходному состоянию"
+    resetBtn.addEventListener('click', () => {
+        for (let i = 0; i < elemsdoll_7_.length; i++) {
+            elemsdoll_7[i].classList.add("hide");
+            elemsdoll_7[i].style.position = 'relative ';
+            elemsdoll_7[i].style.top = '0px';
+            elemsdoll_7[i].style.left = '0px';
+            insideBox.append(elemsdoll_7_[i]);
+            // itemCounter();
+        }
+        removePointerEventsDraggableElems();
+        insideBox.addEventListener('mouseenter', showOpeningBox);
+        insideBox.addEventListener('mouseleave', fadeOpeningBox);
+        insideBox.addEventListener('click', openBox);
+        fadeOpeningBox();
+
+        if (winOpenBox) {
+            winTextSwitcher();
+            winOpenBox = false;
+        }
+    });
+
+
+    // Обработчик кнопки "Полный экран"
+    soundBtfullScreenBtn.addEventListener('click', function (event) {
+        if (wrapper.classList.contains(contentBlocker)) {
+            wrapper.classList.remove(contentBlocker);
+            document.getElementsByTagName('body')[0].style.overflowY = null;
+        } else {
+            wrapper.classList.add(contentBlocker);
+            document.getElementsByTagName('body')[0].style.overflowY = "hidden";
+        }
+        if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+        } else {
+            document.documentElement.requestFullscreen();
+        }
+    }, false);
+
+
+
+    function changeStylesAndAppend(dropPlace, draggingElem) {
+        draggingElem.style.position = 'relative ';
+        draggingElem.style.zIndex = null;
+        draggingElem.style.top = null;
+        draggingElem.style.left = null;
+        dropPlace.appendChild(draggingElem);
+    }
+
+
+    let draggingItem;
+    let elemBelow;
+
+
+    function mouseDown(event) {
+        if (event.button !== 0) return;
+        draggingItem = event.target;
+        draggingItem.style.cursor = "url(Images_1/doll_7_img/cursor.png), auto";
+        const elemDraggingBanBorder = divMain; //элемент за границы которого запрещён вылет перетаскиваемой фигуры
+        const elemDraggingStartPlace = insideBox; //элемент первоначального расположения перетаскиваемых фигур (стартовое состояние)
+
+        draggingItem.style.touchAction = 'none'; //ОБЯЗАТЕЛЬНОЕ УСЛОВИЕ(МОЖНО УБРАТЬ И ПРОПИСАТЬ В СТИЛЬ САМОМУ ОБЪЕКТУ) 
+
+
+        let shiftX = event.clientX - draggingItem.getBoundingClientRect().left;
+        let shiftY = event.clientY - draggingItem.getBoundingClientRect().top;
+
+        // ЛИММИТЫ КООРДИНАТ ОГРАНИЧИВАЮЩИЕ ВЫЛЕТ ПЕРЕТАСКИВАЕМОГО ЭЛЕМЕНТА ЗА БЛОК
+        //  (ПО УМОЛЧАНИЮ interact_zadanie - РОДИТЕЛЬ ВАШЕГО БЛОКА)
+        let limits = {
+            top: elemDraggingBanBorder.offsetTop + scrollY,
+            right: elemDraggingBanBorder.offsetWidth + elemDraggingBanBorder.offsetLeft + scrollX,
+            bottom: elemDraggingBanBorder.offsetHeight + elemDraggingBanBorder.offsetTop + scrollY,
+            left: elemDraggingBanBorder.offsetLeft + scrollX
+        };
+
+        draggingItem.style.position = 'absolute';
+        draggingItem.style.zIndex = 1000;
+        document.body.appendChild(draggingItem);
+
+        moveAt(event.pageX, event.pageY);
+
+        function moveAt(pageX, pageY) {
+            draggingItem.style.left = pageX - shiftX + 'px';
+            draggingItem.style.top = pageY - shiftY + 'px';
+        }
+
+        elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+
+        let clickWithoutMove = true;
+
+        function onMouseMove(event) {
+            let newLocation = {
+                x: limits.left,
+                y: limits.top
+            };
+            if (event.pageX > limits.right) {
+                newLocation.x = limits.right;
+            } else if (event.pageX > limits.left) {
+                newLocation.x = event.pageX;
+            }
+            if (event.pageY > limits.bottom) {
+                newLocation.y = limits.bottom;
+            } else if (event.pageY > limits.top) {
+                newLocation.y = event.pageY;
+            }
+
+            clickWithoutMove = false
+            moveAt(newLocation.x, newLocation.y);
+            // moveAt(event.pageX, event.pageY);
+
+            if (!event.path.includes(draggingItem)) {
+                window.addEventListener('pointerup', moveOut);
+            }
+            if (event.path.includes(draggingItem)) {
+                window.removeEventListener('pointerup', moveOut);
+            }
+
+            draggingItem.hidden = true;
+            elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+            draggingItem.hidden = false;
+
+            if (!elemBelow) return;
+
+            // ОБРАБОТКА СОБЫТИЯ НАХОЖДЕНИЯ НАД БЛОКОМ И ВЫЛЕТА ИЗ НЕГО (ПО НЕОБХОДИМИОСТИ)
+
+            // let currentDroppable = null;
+
+            // let droppableBelow = elemBelow.closest('.droppable'); // БЕРЁМ НУЖНЫЙ БЛОК 
+
+            // if (currentDroppable != droppableBelow) {
+            //     if (currentDroppable) { 
+            // ЛОГИКА ОБРАБОТКИ ПРОЦЕССА "ВЫЛЕТА" ИЗ DROPPABLE
+            //         leaveDroppable(currentDroppable);
+            //     }
+            //     currentDroppable = droppableBelow;
+            // ЛОГИКА ОБРАБОТКИ ПРОЦЕССА, КОГДА МЫ "ВЛЕТАЕМ" В ЭЛЕМЕНТ
+            //     if (currentDroppable) {
+            //         enterDroppable(currentDroppable);
+            //     }
+            // }
+        }
+
+        // КОГДА НАД ВЫБРАННЫМ БЛОКОМ
+        function enterDroppable(currentDroppable) {
+            // currentDroppable
+        }
+        // КОДА ВЫЛЕТЕЛИ ИЗ БЛОКА
+        function leaveDroppable(currentDroppable) {
+            // currentDroppable
+        }
+        document.addEventListener('pointermove', onMouseMove);
+
+
+        // КОГДА ВО ВРЕМЯ ПЕРЕТАСКИВАНИЯ КУРСОР ВЫНЕСЛИ ЗА ПРЕДЕЛЫ ОКНА БРАУЗЕРА И ОТПУСТИЛИ ЗАХВАТ ЭЛЕМЕНТА
+        function moveOut(e) {
+            smoothTransition(draggingItem)
+            setTimeout(() => changeStylesAndAppend(elemDraggingStartPlace, draggingItem), 1000)
+
+            window.removeEventListener('pointerup', moveOut);
+            document.removeEventListener('pointermove', onMouseMove);
+        }
+
+        // КОГДА КУРСОР В ЗОНЕ ДЛЯ ПЕРЕТАСКИВАНИЙ И ПОЛЬЗОВАТЕЛЬ ОТПУСТИЛ ЗАХВАТ ЭЛЕМЕНТА
+        draggingItem.onpointerup = function (e) {
+            startAction = true;
+            if (clickWithoutMove) {
+                smoothTransition(draggingItem)
+                setTimeout(() => changeStylesAndAppend(elemDraggingStartPlace, draggingItem), 10000)
+
+            }
+
+            document.removeEventListener('pointermove', onMouseMove);
+
+            // ЛОГИКА ОБРАБОТКИ ПОПАДАНИЯ НА НУЖНЫЙ БЛОК И НАОБОРОТ
+            if (elemBelow.classList.contains('doll_7_Main')) {
+                let new_shiftX = (e.clientX - elemBelow.getBoundingClientRect().left) - shiftX - 40 + 'px';
+                let new_shiftY = (e.clientY - elemBelow.getBoundingClientRect().top) - shiftY - 40 + 'px';
+                draggingItem.style.left = new_shiftX;
+                draggingItem.style.top = new_shiftY;
+                elemBelow.appendChild(draggingItem);
+                winFigursCounter();
+            } else {
+                smoothTransition(draggingItem)
+                setTimeout(() => changeStylesAndAppend(insideBox, draggingItem), 1000)
+
+            }
+
+            //winFigursCounter();
+
+        };
+
+        function smoothTransition(draggingElem) {
+            // document.body.style.pointerEvents = 'none'
+            elemsdoll_7_.forEach((e) => {
+                e.removeEventListener('pointerdown', mouseDown);
+            });
+            let coordX,
+                coordY
+            draggingElem.classList.add('dragTransition')
+            coordX = elemDraggingStartPlace.getBoundingClientRect().left + elemDraggingStartPlace.getBoundingClientRect().width / 2
+            coordY = elemDraggingStartPlace.getBoundingClientRect().top + elemDraggingStartPlace.getBoundingClientRect().height / 2 + window.pageYOffset
+            draggingElem.style.left = `${coordX}px`
+            draggingElem.style.top = `${coordY}px`
+            setTimeout(() => {
+                draggingElem.classList.remove('dragTransition')
+                // document.body.style.pointerEvents = 'auto'
+                elemsdoll_7_.forEach((e) => {
+                    e.addEventListener('pointerdown', mouseDown);
+                });
+
+            }, 1000)
+        }
+
+
+    };
+
+
+
+
+    function winFigursCounter() {
+        if (insideBox.getElementsByTagName('img').length == 0) {
+            winOpenBox = true;
+            winTextSwitcher();
+            elemsdoll_7_.forEach((e) => {
+                e.style.pointerEvents = 'none';
+            });
+
+        }
+    };
+
+    
+
+    function winTextSwitcher() {
+
+        if (!well_done.classList.contains('onViewdoll_7')) {
+            // well_done.classList.remove('hide');
+            well_done.classList.add('onViewdoll_7');
+            soundOn && playSound(winSound);
+
+        } else if (well_done.classList.contains('onViewdoll_7')) {
+            // well_done.classList.add('hide');
+            well_done.classList.remove('onViewdoll_7');
+        }
+
+    };
+})();
